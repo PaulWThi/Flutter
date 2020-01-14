74324a1943373284a8878e9bf376f02255dccf98
Paul <Paul.w.thi@gmail.com>
2020-01-13 20:04:58 -0600

    Add More ICE

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -6,10 +6,23 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
+          primary: true,
+          actions: <Widget>[
+            Image(
+              image: AssetImage('images/diamond.png'),
+            ),
+            Image(
+              image: AssetImage('images/diamond.png'),
+            ),
+          ],
           title: Text(
             'Ice Ice Ice Ice',
           ),
           backgroundColor: Colors.blueGrey[900],
+          toolbarOpacity: .5,
+          leading: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
         ),
         body: Center(
           child: Image(
