ae0f49ed5930b59782d21c75dea7bdde9aec8653
Paul <Paul.w.thi@gmail.com>
2020-02-19 20:30:36 -0600

    Progress Commit

--- todoey_flutter/lib/widgets/task_tile.dart
+++ todoey_flutter/lib/widgets/task_tile.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('flex'),
-      trailing: Checkbox(value: false),
+      trailing: TaskCheckbox(isChecked),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+
+  TaskCheckbox(this.checkboxState);
+
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: (newValue) {
+//        setState(() {
+//          isChecked = newValue;
+//        });
+      },
     );
   }
 }
