cc9ff834dffb22cd638617a409798802f1e33354
Paul <Paul.w.thi@gmail.com>
2020-02-22 00:04:57 -0600

    Add Provider Package

---
+++ todoey_flutter/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final String data = "some data";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(data),
+        ),
+        body: Level1(data),
+      ),
+    );
+  }
+}
+
+class Level1 extends StatelessWidget {
+  final String data;
+  Level1(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Level2(data),
+    );
+  }
+}
+
+class Level2 extends StatelessWidget {
+  final String data;
+  Level2(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(),
+        Level3(data),
+      ],
+    );
+  }
+}
+
+class Level3 extends StatelessWidget {
+  final String data;
+  Level3(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(data),
+    );
+  }
+}
